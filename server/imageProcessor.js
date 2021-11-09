f8b30eddeed7405ccee8b547434f919366544591
Tsige Solomon <tsigesolomon@tsiges-mbp.acasmart.jh.edu>
2021-11-08 19:33:15 -0500

    ready to deploy

--- server/imageProcessor.js
+++ server/imageProcessor.js
@@ -1,2 +1,38 @@
 // TODO Write and export a function to perform 
 //   the required image processing task(s)
+const sharp = require("sharp");
+const fs = require("fs");
+const ApiError = require("./ApiError");
+
+
+async function imageProcessor({uni, color, size}) {
+    let path = __dirname + "/../assets/" + uni;
+    size = parseInt(size);
+    try{
+        fs.existsSync(path);
+    } catch(err) {
+        throw new ApiError(400, "We do not have logos for this university");
+    }
+
+    path += "/" + color + ".png";
+    
+    try{
+        fs.existsSync(path);
+    } catch(err) {
+        throw new ApiError(400, "We do not have a logo in this color.");
+    }
+
+    if (size < 20 || size > 500) {
+        throw new ApiError(400, "We can not support this image size.");
+    }
+
+    //open file
+    const buffer = sharp(path)
+        .resize(size)
+        .toBuffer()
+        .then()
+        .catch(() => { throw new ApiError(400, "We can not support " + uni + "/" + color + "/" + size);});
+    return buffer;
+}
+
+module.exports = imageProcessor;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,43 @@
 // TODO Write a server program using Node and Express
+//import imageProcessor from "./imageProcessor";
+const express = require("express");
+const imageProcessor = require("./imageProcessor");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    //res.send("color");
+    res.send("Welcome to the University Logo API!" +
+    "<br/>This simple API can be used to get a university's logo as an image file." +
+    "<br/>The following endpoint is available:\nGET /:uni/:color/:size" +
+    "<br/>* uni is the colloquial abbreviated name of a university, such as JHU for Johns Hopkins University." +
+    "<br/>* color is a color name such as black or blue." +
+    "<br/>* size is a value between 20 to 500 (inclusive) that specifies the image dimensions (size-by-size)"+
+    "<br/><br/>Example: /jhu/blue/150");
+    
+});
+
+
+app.get("/:uni/:color/:size", async (req, res) => {
+    const { uni, color, size } = req.params;
+    try{    
+      const data = await imageProcessor({uni, color, size});
+      res.status(200);
+      res.setHeader('Content-disposition', 'inline; filename=' + `${uni}-${color}-${size}-by-${size}.png`);
+      res.writeHead(200, {"Content-Type": "image/png", "Content-Length": data.length});
+      res.end(data);
+    } catch(err) {
+      res.status(err.status).json({message: err.message});
+    }
+    
+  });
+
+
+  app.all("*", (req, res) => {
+    res.status(404).send("GET " + req.url + " is not supported.");
+  });
+
+app.listen(port, () => {
+    console.log(`Express app listening at port: http://localhost:${port}/`);
+  });
+
